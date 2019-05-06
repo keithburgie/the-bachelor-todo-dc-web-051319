ca21e8c2c3ecab53264f73a15fca1568c43eebbb
Keith Burgie <krburgie@gmail.com>
2019-05-06 02:59:08 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,5 +45,13 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data.each do |year, contestants|
+      if year.to_s == season
+          contestants.each do |contestant|
+              ages << contestant["age"].to_i
+          end
+      end
+  end
+  ages.sum / ages.size
 end
